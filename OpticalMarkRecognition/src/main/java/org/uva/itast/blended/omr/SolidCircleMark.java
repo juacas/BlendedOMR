19ddfd16ef140a1a0a799bacdd9d31d26b1b99bf
juacas <juacas@95dcb3f6-ae72-456f-a756-099e62310317>
2009-05-11 18:31:49 +0000

    Refactoring for fixing several bugs and some styling

--- OpticalMarkRecognition/src/main/java/org/uva/itast/blended/omr/SolidCircleMark.java
+++ OpticalMarkRecognition/src/main/java/org/uva/itast/blended/omr/SolidCircleMark.java
@@ -10,6 +10,7 @@ import org.apache.commons.logging.LogFactory;
 //import net.sourceforge.jiu.codecs.*;
 import java.awt.Color;
 import java.awt.Graphics;
+import java.awt.Polygon;
 import java.awt.image.BufferedImage;
 
 import omrproj.ConcentricCircle;
@@ -26,6 +27,11 @@ import net.sourceforge.jiu.data.*;
 public class SolidCircleMark
 {
 	/**
+	 * 
+	 */
+	private static final int	SCAN_DELTA_DIVISOR	= 4;
+
+	/**
 	 * Logger for this class
 	 */
 	private static final Log	logger			= LogFactory.getLog(SolidCircleMark.class);
@@ -33,7 +39,7 @@ public class SolidCircleMark
 	/**
 	 * 
 	 */
-	private static final double	SCAN_PERCENT	= 0.3;	// porcentaje del radio
+	private static final double	SCAN_PERCENT	= 0.5d;	// porcentaje del radio
 														// para el barrido que
 														// se da para buscar la
 														// marca
@@ -48,6 +54,8 @@ public class SolidCircleMark
 	int							maxsimX, maxsimY;
 
 	private PageImage	pageImage;
+
+	private double	autoSimilarity;
 /**
  * 
  * @param grayimage
@@ -64,19 +72,12 @@ public class SolidCircleMark
 		this.approxXscale = approxXscale;
 		this.approxYscale = approxYscale;
 		this.markHeight= markHeight;
-		
 		this.markWidth=markWidth;
-		// markradX = (int)(ConcentricCircle.markDiam / 2 * approxXscale);
-		// markradY = (int)(ConcentricCircle.markDiam / 2 * approxYscale);
-
-		// TODO: Estaba usando 0!!
-		//markWidth = 5;// XXX utilizar los leídos en el archivo
-		//markHeight = 5;
-
+	
 		template = new MemoryGray8Image((int) (markWidth  * 1.15) + 1,
 				(int) (markHeight  * 1.15) + 1);
 		fillTemplate(template, markWidth/2, approxXscale / approxYscale);
-		//ImageUtil.saveImage(template, "c:\\temp\\sys\\marktemplate.png");
+	
 	}
 
 	private void fillTemplate(Gray8Image templateimg, int markradX,
@@ -84,6 +85,7 @@ public class SolidCircleMark
 	{
 		double centerX = templateimg.getWidth() / 2;
 		double centerY = templateimg.getHeight() / 2;
+		int whites=0;
 		for (int i = 0; i < templateimg.getWidth(); i++)
 		{
 			for (int j = 0; j < templateimg.getHeight(); j++)
@@ -93,12 +95,17 @@ public class SolidCircleMark
 				if (dist <= markradX)
 				{
 					templateimg.putBlack(i, j);
+					
 				} else
 				{
 					templateimg.putWhite(i, j);
+					whites++;
 				}
 			}
 		}
+		
+		// compute autoSimilarity
+		autoSimilarity=((double)whites)/(templateimg.getHeight()*templateimg.getWidth());
 	}
 /**
  * 
@@ -115,8 +122,8 @@ public class SolidCircleMark
 // [JPC] this loop was refactored to start the analysis in the center
 		int maxDeltaX = (int) (markWidth  * SCAN_PERCENT);
 		int maxDeltaY = (int) (markHeight * SCAN_PERCENT);
-		int deltaXY = Math.max(1, markWidth / 5);
-		
+		int deltaXY = Math.max(1, markWidth / SCAN_DELTA_DIVISOR);
+		boolean markpoint=true;// for debugging the position of the templates.
 		for (int i = x; i <= x + maxDeltaX; i += deltaXY)
 		{
 			
@@ -126,7 +133,7 @@ public class SolidCircleMark
 						grayimage, 
 						i - template.getWidth()/2, 
 						j - template.getHeight()/2, template,dump);
-				markPointInImage(i, j);
+				if (markpoint) markPointInImage(i, j);
 				if (maxsim == -1 || maxsim < similarity)
 				{
 					maxsim = similarity;
@@ -136,7 +143,7 @@ public class SolidCircleMark
 				similarity = 1.0 - ConcentricCircle.templateXOR(grayimage, 
 						2* x - i - template.getWidth()/2, 
 						2 * y - j- template.getHeight()/2, template, dump);
-				markPointInImage(2 * x - i, 2 * y - j);
+				if (markpoint) markPointInImage(2 * x - i, 2 * y - j);
 				if (maxsim == -1 || maxsim < similarity)
 				{
 					maxsim = similarity;
@@ -147,7 +154,7 @@ public class SolidCircleMark
 				similarity = 1.0 - ConcentricCircle.templateXOR(grayimage, 
 						i - template.getWidth()/2,
 						2 * y - j - template.getHeight()/2, template, dump);
-				markPointInImage(i, 2 * y - j);
+				if (markpoint)markPointInImage(i, 2 * y - j);
 				if (maxsim == -1 || maxsim < similarity)
 				{
 					maxsim = similarity;
@@ -158,12 +165,13 @@ public class SolidCircleMark
 				similarity = 1.0 - ConcentricCircle.templateXOR(grayimage,
 						2* x - i - template.getWidth()/2,
 						j - template.getHeight()/2, template, dump);
-				markPointInImage(2 * x - i, j);
+				
+				if (markpoint) markPointInImage(2 * x - i, j);
 				if (maxsim == -1 || maxsim < similarity)
 				{
 					maxsim = similarity;
 					maxsimX = 2 * x - i;
-					maxsimY = j;// + markradY * 2; // XXX
+					maxsimY = j;
 				}
 			}
 		}
@@ -173,12 +181,22 @@ public class SolidCircleMark
 			logger
 					.debug("isMark(int, int) - --" + maxsim + ":" + maxsimX + "," + maxsimY + "->" + x + ":" + y); //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$ //$NON-NLS-4$ //$NON-NLS-5$
 		}
-		if (maxsim > 0.55)
+		if (maxsim > getAutoSimilarity()*1.2)
 			return true;
 		else
 			return false;
 
 	}
+
+/**
+ * When comparing with an empty space there are a minimum similarity due to empty space around the mark
+ * @return
+ */
+private double getAutoSimilarity()
+{
+	
+	return autoSimilarity;
+}
 
 	/**
 	 * @param i
@@ -210,20 +228,25 @@ public class SolidCircleMark
 	/**
 	 * @param pageImage
 	 */
-	public void putMarkOnImage(PageImage pageImage)
+	public void putCircleMarkOnImage(PageImage pageImage)
 	{
 		BufferedImage imagen = pageImage.getImagen();
-		Gray8Image grayImg = pageImage.getGrayImage();
+		
 		
 		Graphics g=imagen.createGraphics();
+//		int centerColor=imagen.getRGB(maxsimX, maxsimY);
+//		g.setXORMode(new Color(centerColor));
+//		g.setColor(Color.RED);
+//		g.fillOval(maxsimX - markWidth/2,  maxsimY - markHeight/2, markWidth, markHeight);
+//		g.setPaintMode();
 		g.setColor(Color.RED);
-		g.drawString("PROCESED PAGE: W:"+imagen.getWidth()+" H:"+imagen.getHeight()+"  workcopy W: "+grayImg.getWidth()+" H:"+grayImg.getHeight(), 10, 10);
-		g.setColor(Color.WHITE);
-		g.drawRect(maxsimX - markWidth/2-1,  maxsimY - markHeight/2-1, markWidth, markHeight);
-		g.setColor(Color.RED);
-		g.drawRect(maxsimX - markWidth/2,  maxsimY - markHeight/2, markWidth, markHeight);
-		
-		g.draw3DRect(0, 0, grayImg.getWidth(), grayImg.getHeight(),	true);
+		g.drawOval(maxsimX - markWidth/2-1,  maxsimY - markHeight/2-1, markWidth+1, markHeight+1);
+		g.drawLine(maxsimX , maxsimY - markHeight/2-1, maxsimX, maxsimY - markHeight/2-20);
+		Polygon arrowHead=new Polygon();
+		arrowHead.addPoint(maxsimX , maxsimY - markHeight/2-1);
+		arrowHead.addPoint(maxsimX-6, maxsimY - markHeight/2-10);
+		arrowHead.addPoint(maxsimX+6, maxsimY - markHeight/2-10);
+		g.fillPolygon(arrowHead);
 		
 	}
 
