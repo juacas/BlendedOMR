4474c362f955e4b18e7879b1db71ed47f2299040
juacas <juacas@95dcb3f6-ae72-456f-a756-099e62310317>
2011-07-11 11:43:03 +0000

    Linux script does not honour the spaces in  parameters. FIXED.

--- src/main/resources/blended_omr.sh
+++ src/main/resources/blended_omr.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 script_path=`dirname $0`
-java -cp $script_path/lib -jar $script_path/${project.build.finalName}.jar $*
+java -cp $script_path/lib -jar $script_path/${project.build.finalName}.jar ${1+$@}
