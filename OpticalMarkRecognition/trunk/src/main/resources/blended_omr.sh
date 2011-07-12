14b12522d0e513687703e940f78e673a7de65289
davfer <davfer@95dcb3f6-ae72-456f-a756-099e62310317>
2011-07-12 10:59:07 +0000

    Typo en OMRProcesor. Modificado script_path en blended_omr.sh

--- OpticalMarkRecognition/trunk/src/main/resources/blended_omr.sh
+++ OpticalMarkRecognition/trunk/src/main/resources/blended_omr.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-script_path=`dirname $0`
+#script_path=`dirname $0`
+script_path=/usr/local/src/OpticalMarkRecognition/target/install/OpticalMarkRecognition
 java -cp $script_path/lib -jar $script_path/${project.build.finalName}.jar ${1+$@}
