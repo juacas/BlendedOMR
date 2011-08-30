38ed6150cd0369deabaefb4110fa9f84bd5056f9
davfer <davfer@95dcb3f6-ae72-456f-a756-099e62310317>
2011-08-30 10:26:41 +0000

    Arreglado paso de parámetros entre .sh y .jar

--- src/main/resources/blended_omr.sh
+++ src/main/resources/blended_omr.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 #script_path=`dirname $0`
+#script_path=/home/idelab/workspace/PFC_OpticalMarkRecognition/target/install/OpticalMarkRecognition
 script_path=/usr/local/src/OpticalMarkRecognition/target/install/OpticalMarkRecognition
-java -cp $script_path/lib -jar $script_path/${project.build.finalName}.jar ${1+$@}
+java -cp $script_path/lib -jar $script_path/${project.build.finalName}.jar "$@"
